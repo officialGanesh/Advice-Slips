b7ef7006b34064c405faef2839624f11d1eb7931
Ganesh <ganuofficial2795@gmail.com>
2021-05-24 13:06:02 +0530

    second json data

--- main.py
+++ main.py
@@ -21,9 +21,33 @@ def Random_advice(url):
     except Exception as e:
         print('Something Went Wrong ',e)
 
+def Advice_by_query():
+    '''Getting an advice using id'''
 
+    base_url = 'https://api.adviceslip.com/advice/search/'
+    query = 0
+    
+    
+    try:
+        
+        for i in range(5):
+            query += 1
+            url = f'{base_url}{query}'
+            req = requests.get(url)
+            req.raise_for_status
+
+            # Storing the json data
+            with open(f'Json_data_2/advice{i}.json','w') as f:
+                source = req.json()
+
+                f.write(json.dumps(source,indent=2))
+            
+    except Exception as e:
+        print('Something Went Wrong ',e)
 
 if __name__ == "__main__":
 
-    Random_advice(END_POINT_1)    
+    Random_advice(END_POINT_1)  
+    Advice_by_query()  
+
     print('Code Completed 🔥')
